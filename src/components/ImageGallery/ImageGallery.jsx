c8e1c162b8af0c07e54ebed792a1f493b5efee45
ewasawczynska <sawczynska.e@gmail.com>
2023-09-25 22:55:20 +0200

    component refactor in progress

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,97 +1,91 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { fetchImagesWithQuery } from 'services/api';
 import { Button, ImageGalleryItem, Loader, Modal } from 'components';
 import '../../styles.css';
 
-export class ImageGallery extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    isModalOpen: false,
-    selectedImage: null,
-    page: 1,
-    totalPages: 0,
+export default function ImageGallery() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [pages, setPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [error, setError] = useState('');
+
+  // useEffect(() => {
+  //   const query = this.props.searchQuery;
+  //   if (query !== prevProps.searchQuery) {
+  //     (setImages([])), setPages(1), () => {
+  //       fetchImages(query, 1);
+  //     }
+  //   }
+  //   if (pages => !pages) {
+  //     loadMoreImages(query, page)
+  //   }
+  // }, [query, pages, fetchImages ]);
+
+  //   useEffect(() => {
+  //   (async fetchImages(query, page) {
+  //     setIsLoading(true)};
+  //     try {
+  //       const response = await fetchImagesWithQuery(query, page);
+  //       const data = response.data.hits;
+  //       const totalPages = Math.floor(response.data.total / 12);
+  //       setImages(data);
+  //       setPages(1);
+  //       setTotalPages(totalPages);
+  //     } catch (error) {
+  //       setError({ error });
+  //     } finally {
+  //       setIsLoading(false );
+  //     })
+  //   }, []);
+
+  //     async loadMoreImages(query, page) {
+  //     setIsLoading(true);
+  //       try {
+  //         const response = await fetchImagesWithQuery(query, page);
+  //         const data = response.data.hits;
+  //         setImages(prev => [...prev, ...data]);
+  //       } catch (error) {
+  //         setError({ error });
+  //       } finally {
+  //         setIsLoading(false );
+  //       }
+  //     }
+
+  const handleLoadMore = () => {
+    setPages(prev => prev + 1);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { page } = this.state;
-    const query = this.props.searchQuery;
-
-    if (this.props.searchQuery !== prevProps.searchQuery) {
-      this.setState({ images: [], page: 1 }, () => {
-        this.fetchImages(query, 1);
-      });
-    }
-
-    if (this.state.page !== prevState.page) {
-      this.loadMoreImages(query, page);
-    }
-  }
-
-  async fetchImages(query, page) {
-    this.setState({ isLoading: true });
-    try {
-      const response = await fetchImagesWithQuery(query, page);
-      const data = response.data.hits;
-      const totalPages = Math.floor(response.data.total / 12);
-      this.setState({
-        images: data,
-        page: 1,
-        totalPages: totalPages,
-      });
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
-
-  async loadMoreImages(query, page) {
-    this.setState({ isLoading: true });
-    try {
-      const response = await fetchImagesWithQuery(query, page);
-      const data = response.data.hits;
-      this.setState(prevState => ({ images: [...prevState.images, ...data] }));
-    } catch (error) {
-      this.setState({ error });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-  }
-
-  handleLoadMore = () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
+  const openModal = image => {
+    setSelectedImage(image);
+    setIsModalOpen(true);
   };
 
-  openModal = image => {
-    this.setState({ selectedImage: image, isModalOpen: true });
+  const closeModal = () => {
+    setSelectedImage(null);
+    setIsModalOpen(false);
   };
 
-  closeModal = () => {
-    this.setState({ selectedImage: null, isModalOpen: false });
-  };
-
-  render() {
-    const { images, isLoading, page, selectedImage, totalPages } = this.state;
-    return (
-      <>
-        <ul className="ImageGallery">
-          {images.map(({ id, ...rest }) => (
-            <ImageGalleryItem key={id} {...rest} onClick={this.openModal} />
-          ))}
-        </ul>
-        {isLoading && <Loader />}
-        {page < totalPages && (
-          <Button onClick={this.handleLoadMore}>Load more</Button>
-        )}
-        {selectedImage && (
-          <Modal
-            largeImageURL={this.state.selectedImage.largeImageURL}
-            tags={this.state.selectedImage.tags}
-            onClick={this.closeModal}
-          />
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <ul className="ImageGallery">
+        {images.map(({ id, ...rest }) => (
+          <ImageGalleryItem key={id} {...rest} onClick={openModal} />
+        ))}
+      </ul>
+      {isLoading && <Loader />}
+      {pages < totalPages && (
+        <Button onClick={handleLoadMore}>Load more</Button>
+      )}
+      {selectedImage && (
+        <Modal
+          largeImageURL={selectedImage.largeImageURL}
+          tags={selectedImage.tags}
+          onClick={closeModal}
+        />
+      )}
+    </>
+  );
 }
